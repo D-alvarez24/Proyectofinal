ae483c217919d74a0653c57362579f0b33b67bed
D-alvarez24 <daviid1108.da@gmail.com>
2024-05-05 21:30:28 -0500

    Mensaje de confirmación

---
+++ node.js
@@ -0,0 +1,21 @@
+
+function registerProduct() {
+    var productForm = document.getElementById("productForm").value;
+    document.getElementById("mensaje").innerText = "Registrando por favor espere...";
+
+}
+
+function mostrarResultado(producto) {
+    var categoria;
+    
+    }
+    document.getElementById("mensaje").innerText = "";
+    document.getElementById("resultado").innerText = "Su producto es " + promedio.toFixed(2) + " Como resultado es " + categoria;
+
+document.getElementById("mensaje").innerText = "";
+
+
+function Limpiar() {
+    document.getElementById("Limpiar").value = '';
+}
+
